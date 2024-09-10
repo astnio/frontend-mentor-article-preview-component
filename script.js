c5182bcf577cdd576036ef9e7f5daf8fed9109a8
austinh-io <github@msg.austinh.io>
2024-09-10 09:45:33 -0700

    Setup test setup in js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ btnShare.addEventListener('click', () => {
 	shareButtonsGroup.dataset.active = shareButtonsActive;
 	btnShare.dataset.active = shareButtonsActive;
 });
+
+// TODO: Remove anything below this when done,
+// it is just for testing!
+
+function test_startSharePopup() {
+	shareButtonsActive = !shareButtonsActive;
+	shareButtonsGroup.dataset.active = shareButtonsActive;
+	btnShare.dataset.active = shareButtonsActive;
+}
+
+test_startSharePopup();
